ef9a2a2b010a943ac27b97184b6faf1d89e1ce14
endy <endy@oceo.me>
2017-07-24 16:05:40 +0800

    test

--- demo/action.php
+++ demo/action.php
@@ -12,34 +12,6 @@ const SECRET_KEY = 'sW0jdqsmp4u69EG4AyXXndWLVBkCqiNg';
 // 初始化
 $aipOcr = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
 
-// 身份证识别
-// echo json_encode($aipOcr->idcard(file_get_contents('idcard.jpg'), true), JSON_PRETTY_PRINT);
-
-// 银行卡识别 
-// echo json_encode($aipOcr->bankcard(file_get_contents('bankcard.jpg')));
-
-// 通用文字识别(含文字位置信息)
-// echo json_encode($aipOcr->general(file_get_contents('general.png')));
-
-
-// 通用文字识别(不含文字位置信息)
-// echo json_encode($aipOcr->basicGeneral(file_get_contents('general.png')), JSON_PRETTY_PRINT);
-
-// 网图OCR识别
-// echo json_encode($aipOcr->webImage(file_get_contents('general.png')), JSON_PRETTY_PRINT);
-
-// 生僻字OCR识别
-// echo json_encode($aipOcr->enhancedGeneral(file_get_contents('general.png')), JSON_PRETTY_PRINT);
-
-// 行驶证识别
-// echo json_encode($aipOcr->vehicleLicense(file_get_contents('vehicleLicense.jpg')), JSON_PRETTY_PRINT);
-
-// 驾驶证
-// echo json_encode($aipOcr->drivingLicense(file_get_contents('drivingLicense.jpg')), JSON_PRETTY_PRINT);
-
-// 车牌
-// echo json_encode($aipOcr->licensePlate(file_get_contents('licensePlate.jpg')), JSON_PRETTY_PRINT);
-
 $file = $_FILES['file'];
 
 //类型
