e296a845b93c197ad036c00807f9add06406af5b
endy <endy@oceo.me>
2017-07-24 15:27:47 +0800

    test

--- demo/index.php
+++ demo/index.php
@@ -1,41 +1,24 @@
-<?php
-
-// 引入文字识别OCR SDK
-require_once '../AipOcr.php';
-
-// 定义常量
-const APP_ID = '9923767';
-const API_KEY = 'M0NyZq47eoaoF1rALB8ntR74';
-const SECRET_KEY = 'sW0jdqsmp4u69EG4AyXXndWLVBkCqiNg';
-
-
-// 初始化
-$aipOcr = new AipOcr(APP_ID, API_KEY, SECRET_KEY);
-
-// 身份证识别
-// echo json_encode($aipOcr->idcard(file_get_contents('idcard.jpg'), true), JSON_PRETTY_PRINT);
-
-// 银行卡识别 
-// echo json_encode($aipOcr->bankcard(file_get_contents('bankcard.jpg')));
-
-// 通用文字识别(含文字位置信息)
-echo json_encode($aipOcr->general(file_get_contents('general.png')));
-
-
-// 通用文字识别(不含文字位置信息)
-// echo json_encode($aipOcr->basicGeneral(file_get_contents('general.png')), JSON_PRETTY_PRINT);
-
-// 网图OCR识别
-// echo json_encode($aipOcr->webImage(file_get_contents('general.png')), JSON_PRETTY_PRINT);
-
-// 生僻字OCR识别
-// echo json_encode($aipOcr->enhancedGeneral(file_get_contents('general.png')), JSON_PRETTY_PRINT);
-
-// 行驶证识别
-// echo json_encode($aipOcr->vehicleLicense(file_get_contents('vehicleLicense.jpg')), JSON_PRETTY_PRINT);
-
-// 驾驶证
-// echo json_encode($aipOcr->drivingLicense(file_get_contents('drivingLicense.jpg')), JSON_PRETTY_PRINT);
-
-// 车牌
-// echo json_encode($aipOcr->licensePlate(file_get_contents('licensePlate.jpg')), JSON_PRETTY_PRINT);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>图片识别</title>
+</head>
+<body>
+	<form action="action.php" method="post" enctype="multipart/form-data">
+		<select name="type"> 
+			<option value="1">通用文字(不含位置信息)</option> 
+			<option value="2">通用文字(含文字位置信息)</option>
+			<option value="3">身份证</option> 
+			<option value="4">银行卡</option> 
+			<option value="5">网图OCR</option> 
+			<option value="6">生僻字</option>
+			<option value="7">行驶证</option> 
+			<option value="8">驾驶证</option>
+			<option value="9">车牌</option> 
+		</select> 
+		<input type="file" value="" name="file">
+		<input type="submit" value='提交'>
+	</form>
+</body>
+</html>
